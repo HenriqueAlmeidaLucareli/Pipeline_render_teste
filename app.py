bb42c46bb818e551bab10fcfd5f5dddefcfb8685
HenriqueAlmeidaLucareli <henrique.lucareli@germinare.org.br>
2024-09-14 10:02:45 -0300

    V1 projeto flask

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# Lista de animes inicial
+animes = [
+    {"nome": "Naruto", "genero": "Ação", "episodios": 500},
+    {"nome": "One Piece", "genero": "Aventura", "episodios": 1000},
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html', animes=animes)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_anime():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        genero = request.form['genero']
+        episodios = request.form['episodios']
+        animes.append({"nome": nome, "genero": genero, "episodios": episodios})
+        return redirect(url_for('index'))
+    return render_template('add_anime.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
